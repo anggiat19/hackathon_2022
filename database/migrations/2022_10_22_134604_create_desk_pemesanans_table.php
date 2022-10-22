027e94b0f5ca799e57a3ba0ff272db00878d515c
syahlan1i <msyahlan336@example.com>
2022-10-22 21:01:18 +0700

    s

--- database/migrations/2022_10_22_134604_create_desk_pemesanans_table.php
+++ database/migrations/2022_10_22_134604_create_desk_pemesanans_table.php
@@ -15,6 +15,8 @@ return new class extends Migration
     {
         Schema::create('desk_pemesanans', function (Blueprint $table) {
             $table->id();
+            $table->double('jumlah_beli');
+            $table->double('jumlah_harga');
             $table->timestamps();
         });
     }
